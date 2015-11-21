61e87ab84f0c06d33b184c261946b5e9e849240d
chico <chi187@gmail.com>
2015-11-21 17:33:11 +0300

    release

---
+++ README.md
@@ -0,0 +1,83 @@
+# Ambry
+
+Ambry is WebStorage wrapper that makes your life easier.
+
+You can create storages with namespaces, control scheme version and subscribe to particular storage.
+
+### Installation
+
+```
+npm install --save ambry
+```
+
+### Creating storage
+
+```js
+import { createStorage } from 'ambry';
+
+const storage = createStorage(config);
+```
+
+`config` consists of:
+- `type` — should be `'localStorage'` or `'sessionStorage'` (Default value: `'localStorage'`)
+- `namespace` - prefix for your storage in storage key field (Default value: `null`)
+- `schemeVersion` - your scheme version wich is useful in cases your storage structure could change over time (Default value: `null`)
+- `clearOnError` - if `true` clears field when parsing for `getItem()` fails or when `setItem()` is impossible because of storage quota has exceede (Default value: `true`)
+- `doNotThrow` - if `true` then creates dummy storage instead of throwing error. Dummy storage has all methods that standard storage has (Default value: `true`)
+
+### Setting new value
+
+```js
+storage.setItem(field, value);
+```
+
+If quota is exceeded and you have `clearOnError` set to `true` then storage will be cleared, then scheme version will be set if needed and then `setItem()` will be called another time
+
+### Getting value
+
+```js
+storage.getItem(field);
+```
+
+If value for that field can't be parsed and `clearOnError` is set to `true` then that value will be removed from storage
+
+### Removing value
+
+```js
+storage.removeItem(field);
+```
+
+### Clearing storage
+
+```js
+storage.clear();
+```
+
+### Changing scheme version
+
+```js
+storage.setSchemeVersion(version);
+```
+
+This will clear storage if previous version is not strictly equal to new version
+
+### Subscribing to storage changes
+
+```js
+const unsubscribe = storage.subscribe(handler);
+```
+
+This will subscribe to that particular storage changes only. It returns new function which when called will unsubscribe your handler.
+
+The `handler` is callback function which has such parameters:
+- `key` - the field name inside of storage
+- `oldValue` - previous value for that key
+- `newValue` - new value for that key
+
+### Converting existing localStorage or sessionStorage data to Ambry storage
+
+If you have Web Storage in your app you can just `createStorage()` to work with it as with Ambry storage. You can even split your storage data to multiple Ambry storages using `namespace`
+
+## Contributing
+
+Your contributions are always welcome! Please feel free to create issues.
--- src/index.js
+++ src/index.js
@@ -10,44 +10,45 @@ class StorageDummy {
   getItem() {}
   removeItem() {}
   setItem() {}
+  subscribe() {}
 }
 
 const subscribes = {};
+
+function isQuotaExceeded(e) {
+  if (e) {
+    if (e.code) {
+      switch (e.code) {
+      case 22:
+        return true;
+      case 1014:
+        // Firefox
+        return e.name === FIREFOX_ERROR_NAME;
+      default:
+        return false;
+      }
+    } else if (e.number === IE8_ERROR_MAGIC_NUMBER) {
+      // Internet Explorer 8
+      return true;
+    }
+  }
+  return false;
+}
 
 // TODO fixes for IE10-11
 // ref http://caniuse.com/#feat=namevalue-storage
 // known issues 6, 7
 
 class Storage extends StorageDummy {
-  constructor({ storage, namespace, schemeVersion, clearOnExceed }) {
+  constructor({ storage, namespace, schemeVersion, clearOnError }) {
     super();
     this._storage = storage;
     this._prefix = namespace ? `${namespace}.` : '';
-    this._clearOnExceed = clearOnExceed;
-    this._schemeVersion = this.getItem(SCHEME_VERSION_FIELD_NAME);
+    this._clearOnError = clearOnError;
     this.setSchemeVersion(schemeVersion);
   }
-  static isQuotaExceeded(e) {
-    if (e) {
-      if (e.code) {
-        switch (e.code) {
-        case 22:
-          return true;
-        case 1014:
-          // Firefox
-          return e.name === FIREFOX_ERROR_NAME;
-        default:
-          return false;
-        }
-      } else if (e.number === IE8_ERROR_MAGIC_NUMBER) {
-        // Internet Explorer 8
-        return true;
-      }
-    }
-    return false;
-  }
   setSchemeVersion(version) {
-    if (this._schemeVersion !== version) {
+    if (this.getItem(SCHEME_VERSION_FIELD_NAME) !== version) {
       this.clear();
       this.setItem(SCHEME_VERSION_FIELD_NAME, version);
     }
@@ -67,6 +68,9 @@ class Storage extends StorageDummy {
       return JSON.parse(this._storage.getItem(this.prefixName(field)));
     } catch (e) {
       console.warn(e);
+      if (this._clearOnError) {
+        this.removeItem(field);
+      }
       return {};
     }
   }
@@ -82,23 +86,31 @@ class Storage extends StorageDummy {
       this._storage.setItem(this.prefixName(field), JSON.stringify(data));
       return data;
     } catch (e) {
-      if (Storage.isQuotaExceeded(e)) {
+      if (isQuotaExceeded(e)) {
         console.warn('Quote is exceeded.');
-        if (this._clearOnExceed) {
+        if (this._clearOnError) {
           console.warn('Clearing storage.');
+          const schemeVersion = this.getItem(SCHEME_VERSION_FIELD_NAME);
           this.clear();
+          if (schemeVersion !== null) {
+            this.setItem(SCHEME_VERSION_FIELD_NAME, schemeVersion);
+          }
           this._storage.setItem(this.prefixName(field), JSON.stringify(data));
           return data;
         }
       }
     }
   }
-  // TODO make it possible to do multiple subscribes
   subscribe(fn) {
-    subscribes[this._prefix] = fn;
-  }
-  unsubscribe() {
-    delete subscribes[this._prefix];
+    if (!subscribes[this._prefix]) {
+      subscribes[this._prefix] = [];
+    }
+    const subscribesOfNamespace = subscribes[this._prefix];
+    subscribesOfNamespace.push(fn);
+    return function unsubscribe() {
+      const index = subscribesOfNamespace.indexOf(fn);
+      subscribes.splice(index, 1);
+    };
   }
 }
 
@@ -116,12 +128,11 @@ function isStorageAvailable(storage) {
 window.addEventListener('storage', ({ key, oldValue, newValue }) => {
   Object.keys(subscribes).forEach(prefix => {
     if (key.indexOf(prefix) === 0) {
-      subscribes[prefix](
+      subscribes[prefix].forEach(subscribe => subscribe(
         key.replace(prefix, ''),
-        // TODO warn if parsing fails
         JSON.parse(oldValue),
         JSON.parse(newValue)
-      );
+      ));
     }
   });
 }, false);
@@ -130,12 +141,12 @@ export function createStorage({
   type = 'localStorage',
   namespace = null,
   schemeVersion = null,
-  clearOnExceed = true,
+  clearOnError = true,
   doNotThrow = true,
-}) {
+} = {}) {
   const storage = window[type];
   if (storage && isStorageAvailable(storage)) {
-    return new Storage({storage, schemeVersion, namespace, clearOnExceed});
+    return new Storage({storage, schemeVersion, namespace, clearOnError});
   }
   if (doNotThrow) {
     console.warn(`WebStorages are not available in current environment.`);
